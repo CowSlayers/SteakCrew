0845fb68af9f9ec882c4c66db4905aafe559ca92
Corey Tan <polarbearlu968@gmail.com>
2021-12-04 13:02:26 -0800

    exploding arrows

--- cows.py
+++ cows.py
@@ -23,11 +23,11 @@ class CowShooter(gym.Env):
 
     def __init__(self, env_config):  
         # Static Parameters
-        self.size = 20
-        self.cow_density = .01
-        self.mushroom_cow_density = .002
-        self.obstacle_density = .05
-        self.obs_size = 40
+        self.size = 25
+        self.cow_density = .015
+        self.mushroom_cow_density = .004
+        self.obstacle_density = .1
+        self.obs_size = 50
         self.max_episode_steps = 400
         self.log_frequency = 10
         self.action_dict = {
@@ -36,12 +36,12 @@ class CowShooter(gym.Env):
             2: 'turn -1',  # Turn 90 degrees to the left
             3: 'attack 1'  # Destroy block
         }
+        self.shots_taken = 0
+        self.init = 0
 
         # Rllib Parameters
         self.action_space = Box(-1, 1, shape=(3,), dtype=np.float32) #Discrete(len(self.action_dict)) 
         self.observation_space = Box(0, 1, shape=(2 * self.obs_size * self.obs_size, ), dtype=np.float32)
-        print(self.action_space)
-        print(self.observation_space)
 
         # Malmo Parameters
         self.agent_host = MalmoPython.AgentHost()
@@ -76,6 +76,8 @@ class CowShooter(gym.Env):
         self.steps.append(current_step + self.episode_step)
         self.episode_return = 0
         self.episode_step = 0
+        self.shots_taken = 0
+        self.init = 0
 
         # Log
         if len(self.returns) > self.log_frequency + 1 and \
@@ -101,10 +103,117 @@ class CowShooter(gym.Env):
             info: <dict> dictionary of extra information
         """
 
+        #initial setup
+        if self.init == 0:
+            from pynput import keyboard
+            from pynput import mouse
+
+            self.agent_host.sendCommand("chat /gamemode creative")
+            self.agent_host.sendCommand("chat /weather rain")
+            self.agent_host.sendCommand("chat /scoreboard objectives add inGround dummy inGround")
+
+            #use command block
+            self.agent_host.sendCommand("setYaw -90")
+            self.agent_host.sendCommand("setPitch 30")
+
+            #########
+            #block 1#
+            #########
+            time.sleep(0.5)
+            self.agent_host.sendCommand("use 1")
+            time.sleep(0.5)
+            self.agent_host.sendCommand("use 0")
+
+            #enter commands
+            key = keyboard.Controller()
+            for char in "execute @e[type=Arrow] ~ ~ ~ scoreboard players set @e[c=1,r=0,type=Arrow] inGround 1 {inGround:1b}":
+                key.press(char)
+                key.release(char)
+                #time.sleep(0.08)
+            
+            #mouse simulation
+            m = mouse.Controller()
+            m.position = (750, 680)
+            time.sleep(0.2)
+            m.click(mouse.Button.left, 2)
+            time.sleep(0.2)
+            m.position = (1180, 680)
+            time.sleep(0.2)
+            m.click(mouse.Button.left, 1)
+            time.sleep(0.2)
+            m.position = (750, 750)
+            time.sleep(0.2)
+            m.click(mouse.Button.left, 1)
+            time.sleep(0.2)
+
+            #########
+            #block 2#
+            #########
+            self.agent_host.sendCommand("tpz -1")
+            time.sleep(0.5)
+            self.agent_host.sendCommand("use 1")
+            time.sleep(0.5)
+            self.agent_host.sendCommand("use 0")
+
+            key = keyboard.Controller()
+            for char in "execute @e[type=Arrow,score_inGround_min=1] ~ ~ ~ summon tnt ~ ~ ~ {Fuse:0}":
+                key.press(char)
+                key.release(char)
+                #time.sleep(0.08)
+
+             #mouse simulation
+            m = mouse.Controller()
+            m.position = (750, 680)
+            time.sleep(0.2)
+            m.click(mouse.Button.left, 1)
+            time.sleep(0.2)
+            m.position = (1180, 680)
+            time.sleep(0.2)
+            m.click(mouse.Button.left, 1)
+            time.sleep(0.2)
+            m.position = (750, 750)
+            time.sleep(0.2)
+            m.click(mouse.Button.left, 1)
+            time.sleep(0.2)
+
+            #########
+            #block 3#
+            #########
+
+            self.agent_host.sendCommand("tpz -2")
+            time.sleep(0.5)
+            self.agent_host.sendCommand("use 1")
+            time.sleep(0.5)
+            self.agent_host.sendCommand("use 0")
+
+            key = keyboard.Controller()
+            for char in "kill @e[type=Arrow,score_inGround_min=1]":
+                key.press(char)
+                key.release(char)
+                #time.sleep(0.08)
+
+             #mouse simulation
+            m = mouse.Controller()
+            m.position = (750, 680)
+            time.sleep(0.2)
+            m.click(mouse.Button.left, 1)
+            time.sleep(0.2)
+            m.position = (1180, 680)
+            time.sleep(0.2)
+            m.click(mouse.Button.left, 1)
+            time.sleep(0.2)
+            m.position = (750, 750)
+            time.sleep(0.2)
+            m.click(mouse.Button.left, 1)
+            time.sleep(0.2)
+
+            self.agent_host.sendCommand("setPitch 4")
+
+            self.init = 1
+
         # Get Action
         #[move, turn, attack]
         #sample -> [-1, -1, 0.06] <- npfloat32
-        
         move = action[0]
         turn = action[1]
         attack = action[2]
@@ -122,31 +231,40 @@ class CowShooter(gym.Env):
             self.agent_host.sendCommand(turn_command)
             time.sleep(.2)
             self.episode_step += 1
-        elif len(self.allow_shoot_action) == 6: #from nearby entities
+        elif len(self.allow_shoot_action) == 7: #found beef/leather
+            print("WALK TOWARDS BEEF/LEATHER")
+            agent_vector = [self.allow_shoot_action[1], self.allow_shoot_action[2]]
+            bl_vector = [self.allow_shoot_action[3], self.allow_shoot_action[4]]
+            direction_vector = [bl_vector[0] - agent_vector[0], bl_vector[1] - agent_vector[1]]
+            x = direction_vector[0]
+            z = direction_vector[1]
+            radians = math.atan2(direction_vector[1],direction_vector[0])
+            degrees = radians * 180 / math.pi
+            yaw = degrees - 90
+            self.agent_host.sendCommand("setYaw {}".format(yaw))
+
+            distance = math.sqrt((self.allow_shoot_action[4] -  self.allow_shoot_action[2])**2 + (self.allow_shoot_action[3] - self.allow_shoot_action[1])**2)
+            self.agent_host.sendCommand("turn 0")
+            self.agent_host.sendCommand("move 1")
+            time.sleep(2)
+
+
+        elif len(self.allow_shoot_action) == 6: #found cows nearby entities
             print("SHOOTING COWS FROM NEARBY ENTTIY INFORMATION") 
             #needs to turn towards the cow
-            print("current agent position: ", self.allow_shoot_action[1], self.allow_shoot_action[2])
-            print("the cow we're shooting position: ", self.allow_shoot_action[3], self.allow_shoot_action[4])
-
             agent_vector = [self.allow_shoot_action[1], self.allow_shoot_action[2]]
             cow_vector = [self.allow_shoot_action[3], self.allow_shoot_action[4]]
-            direction_vector = [agent_vector[0] - cow_vector[0], agent_vector[1] - cow_vector[1]]
+            direction_vector = [cow_vector[0] - agent_vector[0], cow_vector[1] - agent_vector[1]]
 
             x = direction_vector[0]
             z = direction_vector[1]
             print("atan2: ", math.atan2(direction_vector[1], direction_vector[0]))
             radians = math.atan2(direction_vector[1],direction_vector[0])
 
-            degrees = radians * 180 / 3.14159265358979323
-            degrees += 180
+            degrees = radians * 180 / math.pi
+            
+            yaw = degrees - 90
 
-            print("VECTORS", agent_vector, cow_vector)
-            
-            if 0 <= degrees <= 180: #negative yaw
-                yaw = 0 - degrees
-            else: #positive yaw
-                yaw = 360 - degrees 
-            
             print("shooting at yaw: ", yaw)
             self.agent_host.sendCommand("setYaw {}".format(yaw))
 
@@ -167,6 +285,7 @@ class CowShooter(gym.Env):
             self.agent_host.sendCommand("use 0")
             time.sleep(0.2)
             self.agent_host.sendCommand("setPitch 4")
+            self.shots_taken += 1
             self.episode_step += 1
 
             """
@@ -183,7 +302,7 @@ class CowShooter(gym.Env):
 
             #calculate pitch based off of distance
             #int pitch
-            pitch = 4 - distance/4
+            pitch = 4 - (distance/2)
 
             self.agent_host.sendCommand("setPitch {}".format(pitch))
 
@@ -192,6 +311,7 @@ class CowShooter(gym.Env):
             self.agent_host.sendCommand("use 0")
             time.sleep(0.2)
             self.agent_host.sendCommand("setPitch 4")
+            self.shots_taken += 1
             self.episode_step += 1
 
         # Get Observation
@@ -217,22 +337,20 @@ class CowShooter(gym.Env):
         my_xml = ""
         base_xml = "<DrawEntity x='{}' y='{}' z='{}' type='{}' xVel='1' zVel='1' />"
 
-        """
         cow_probability = int(self.cow_density * 1000)
         mushroom_cow_probability = int(self.mushroom_cow_density * 1000)
         chance = None
         #construct the xml here
         for x in range(-20, 20):
             for z in range(-20, 20):
-                chance = random.randint(1, 1000)
-                if chance <= mushroom_cow_probability:
-                    my_xml += base_xml.format(x, 2, z, 'MushroomCow')
-                elif chance <= cow_probability:
-                    my_xml += base_xml.format(x, 2, z, 'Cow')
-
-        """
-        my_xml += base_xml.format(2, 2, 2, 'Cow')
-
+                if x in [-2,-1,0,1,2] and z in [-2,-1,0,1,2]:
+                    continue
+                else:
+                    chance = random.randint(1, 1000)
+                    if chance <= mushroom_cow_probability:
+                        my_xml += base_xml.format(x, 2, z, 'MushroomCow')
+                    elif chance <= cow_probability:
+                        my_xml += base_xml.format(x, 2, z, 'Cow')
 
         #drawing glass obstacles in my_xml2
         
@@ -240,30 +358,24 @@ class CowShooter(gym.Env):
 
         
         base_xml2 = "<DrawBlock x='{}' y='{}' z='{}' type='{}'/>"
-        """
         obstacle_probability = int(self.obstacle_density * 1000)
         chance = None
         #construct the xml here
-        for x in range(-50, 50):
-            for z in range(-50, 50):
-                chance = random.randint(1, 1000)
-                if chance <= obstacle_probability:
-                    my_xml2 += base_xml2.format(x, 2, z, 'glass')
-                    my_xml2 += base_xml2.format(x, 3, z, 'glass')
-                if chance <= obstacle_probability * 0.75:
-                    my_xml2 += base_xml2.format(x, 4, z, 'glass')
-                if chance <= obstacle_probability * 0.5:
-                    my_xml2 += base_xml2.format(x, 5, z, 'glass')
-                if chance <= obstacle_probability * 0.25:
-                    my_xml2 += base_xml2.format(x, 6, z, 'glass')
-        """
-    
-        
-
-        my_xml2 += base_xml2.format(6, 2, 3, 'glass')
-        my_xml2 += base_xml2.format(6, 3, 3, 'glass')
-        my_xml2 += base_xml2.format(6, 2, 2, 'glass')
-        my_xml2 += base_xml2.format(6, 3, 2, 'glass')
+        for x in range(-20, 20):
+            for z in range(-20, 20):
+                if x in [-2,-1,0,1,2] and z in [-2,-1,0,1,2]:
+                    continue
+                else:
+                    chance = random.randint(1, 1000)
+                    if chance <= obstacle_probability:
+                        my_xml2 += base_xml2.format(x, 2, z, 'glass')
+                        my_xml2 += base_xml2.format(x, 3, z, 'glass')
+                    if chance <= obstacle_probability * 0.75:
+                        my_xml2 += base_xml2.format(x, 4, z, 'glass')
+                    if chance <= obstacle_probability * 0.5:
+                        my_xml2 += base_xml2.format(x, 5, z, 'glass')
+                    if chance <= obstacle_probability * 0.25:
+                        my_xml2 += base_xml2.format(x, 6, z, 'glass')
 
                     
         return '''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
@@ -285,10 +397,13 @@ class CowShooter(gym.Env):
                             <FlatWorldGenerator generatorString="3;7,2;1;"/>
                             <DrawingDecorator>''' + \
                                 "<DrawCuboid x1='{}' x2='{}' y1='2' y2='2' z1='{}' z2='{}' type='air'/>".format(-self.size, self.size, -self.size, self.size) + \
-                                "<DrawCuboid x1='{}' x2='{}' y1='1' y2='1' z1='{}' z2='{}' type='grass'/>".format(-self.size, self.size, -self.size, self.size) + \
+                                "<DrawCuboid x1='{}' x2='{}' y1='1' y2='1' z1='{}' z2='{}' type='bedrock'/>".format(-self.size, self.size, -self.size, self.size) + \
                                 my_xml + \
                                 my_xml2 + \
                                 '''
+                                <DrawBlock x='1' y='2' z='0' type='command_block'/>
+                                <DrawBlock x='1' y='2' z='-1' type='command_block'/>
+                                <DrawBlock x='1' y='2' z='-2' type='command_block'/>
                                 <DrawBlock x='0'  y='2' z='0' type='air' />
                                 <DrawBlock x='0'  y='1' z='0' type='stone' />
                             </DrawingDecorator>
@@ -308,6 +423,7 @@ class CowShooter(gym.Env):
                         </AgentStart>
                         <AgentHandlers>
                             <AbsoluteMovementCommands/>
+                            <ChatCommands/>
                             <ContinuousMovementCommands turnSpeedDegs="150"/>
                             <ObservationFromFullStats/>
                             <ObservationFromRay/>
@@ -318,16 +434,17 @@ class CowShooter(gym.Env):
                                 </Grid>
                             </ObservationFromGrid>
                             <ObservationFromNearbyEntities>
-                                <Range name="entities" xrange="20" yrange="1" zrange="20" update_frequency="20"/>
+                                <Range name="entities" xrange="'''+str(int(self.obs_size/2))+'''" yrange="1" zrange="'''+str(int(self.obs_size/2))+'''" update_frequency="20"/>
                             </ObservationFromNearbyEntities>
                             <RewardForDamagingEntity>
                                 <Mob type="Cow" reward="1"/>
                                 <Mob type="MushroomCow" reward="1"/>
                             </RewardForDamagingEntity>
+                            <RewardForCollectingItem>
+                                <Item type="beef" reward="2"/>
+                                <Item type="leather" reward="2"/>
+                            </RewardForCollectingItem>
                             <AgentQuitFromReachingCommandQuota total="'''+str(self.max_episode_steps)+'''" />
-                            <AgentQuitFromTouchingBlockType>
-                                <Block type="bedrock" />
-                            </AgentQuitFromTouchingBlockType>
                         </AgentHandlers>
                     </AgentSection>
                 </Mission>'''
@@ -405,19 +522,28 @@ class CowShooter(gym.Env):
                         allow_shoot_action = [observations['Yaw'], observations['XPos'], observations['ZPos'], cow['x'], cow['z']]
                         # [cow yaw, agent xpos, agent zpos, cow xpos, cow zpos]
 
+
                     #else check observations
                     elif "entities" in observations:
-                        print("found cows:", observations["entities"])
+                        print("found cows")
                         print("\n")
                         
                         #get positions
                         if world_state.number_of_observations_since_last_state > 0:
                             agent_info = observations["entities"][0]
-                            cows = sorted(observations["entities"][1:], key=lambda x: math.sqrt((x['x']-agent_info['x'])**2 + (x['z']-agent_info['z'])**2))
-                            print("COWS: ", cows)
-                            if len(cows) != 0:
-                                first_cow = cows[0]
-                                allow_shoot_action = [agent_info['yaw'], agent_info['x'], agent_info['z'], first_cow['x'], first_cow['z'], 'True']
+                            beef_and_leather = sorted([ent for ent in observations["entities"][1:] if ent['name'] in ['beef', 'leather']], key=lambda x: math.sqrt((x['x']-agent_info['x'])**2 + (x['z']-agent_info['z'])**2))
+                            if len(beef_and_leather) > 0:
+                                #pick up the beef/leather
+                                first_bl = beef_and_leather[0]
+                                allow_shoot_action = [agent_info['yaw'], agent_info['x'], agent_info['z'], first_bl['x'], first_bl['z'], 'True', 'True']
+                            else:
+                                cows = sorted([ent for ent in observations["entities"][1:] if ent['name'] in ['Cow', 'MushroomCow']], key=lambda x: math.sqrt((x['x']-agent_info['x'])**2 + (x['z']-agent_info['z'])**2))
+                                if len(cows) != 0:
+                                    print("cows? :", cows)
+                                    print()
+                                    print("NUMBER OF COWS nearby: ", len(cows))
+                                    first_cow = cows[0]
+                                    allow_shoot_action = [agent_info['yaw'], agent_info['x'], agent_info['z'], first_cow['x'], first_cow['z'], 'True']
 
                     if type == "glass":
                         print("I SPY GLASS", observations["LineOfSight"])
